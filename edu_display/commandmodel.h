1452b548dfc8b03312466d153a359ef8dad4ac0c
kai-maker <kai1010@outlook.jp>
2019-01-23 13:27:02 +0900

    [fix] add Q_INVOKABLE attribute to some functions

--- edu_display/commandmodel.h
+++ edu_display/commandmodel.h
@@ -24,10 +24,10 @@ public:
     QModelIndex addCommand(const Command& command);
     Q_INVOKABLE void addCommandFromName(const QString& name);
 
-    int rowCount(const QModelIndex& parent = QModelIndex()) const override;
+    Q_INVOKABLE int rowCount(const QModelIndex& parent = QModelIndex()) const override;
     QVariant data(const QModelIndex& index, int role = Qt::DisplayRole) const override;
     bool setData(const QModelIndex& index, const QVariant& value, int role) override;
-    bool removeRows(int row, int count, const QModelIndex& parent) override;
+    Q_INVOKABLE bool removeRows(int row, int count, const QModelIndex& parent = QModelIndex()) override;
     QHash<int, QByteArray> roleNames() const override;
 
 private:
